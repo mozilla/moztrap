23054149228f4da4cdf21ce332243181be297c85
Eric Meyer <eric@oddbird.net>
2011-01-11 15:13:11 -0700

    registration wireframe

--- ui/tcmui/urls.py
+++ ui/tcmui/urls.py
@@ -5,4 +5,5 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns(
     "",
     url("^$", direct_to_template, {"template": "home.html"}, name="home"),
+    url("^account/register/$", direct_to_template, {"template": "account/register.html"}, name="register"),
 )
