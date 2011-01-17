3f195e0a51b31aee87a42c726661be36c6918728
Carl Meyer <carl@oddbird.net>
2011-01-17 17:41:06 -0500

    URL cleanup.

--- ui/tcmui/urls.py
+++ ui/tcmui/urls.py
@@ -6,8 +6,8 @@ urlpatterns = patterns(
     "",
     url("^$", direct_to_template, {"template": "home.html"}, name="home"),
     url("^account/register/$", direct_to_template, {"template": "account/register.html"}, name="register"),
-    url("^test/products/$", direct_to_template, {"template": "test/products.html"}, name="products"),
-    url("^test/product/firefox/environment/$", direct_to_template, {"template": "test/environment.html"}, name="environment"),
-    url("^test/product/firefox/cycles/$", direct_to_template, {"template": "test/cycles.html"}, name="cycles"),
-    url("^test/product/firefox/cycles/1/$", direct_to_template, {"template": "test/run.html"}, name="run"),
+    url("^test/$", direct_to_template, {"template": "test/products.html"}, name="products"),
+    url("^test/product/1/environment/$", direct_to_template, {"template": "test/environment.html"}, name="environment"),
+    url("^test/product/1/cycles/$", direct_to_template, {"template": "test/cycles.html"}, name="cycles"),
+    url("^test/product/1/cycles/1/$", direct_to_template, {"template": "test/run.html"}, name="run"),
 )
