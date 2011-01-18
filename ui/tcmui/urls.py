94eb2aae308564684671bcc9fa9564ad49bac9b0
Eric Meyer <eric@oddbird.net>
2011-01-17 18:08:46 -0700

    create test case wireframe

--- ui/tcmui/urls.py
+++ ui/tcmui/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns(
     url("^test/product/1/environment/$", direct_to_template, {"template": "test/environment.html"}, name="environment"),
     url("^test/product/1/cycles/$", direct_to_template, {"template": "test/cycles.html"}, name="cycles"),
     url("^test/product/1/cycles/1/$", direct_to_template, {"template": "test/run.html"}, name="run"),
+    url("^manage/testcase/add/$", direct_to_template, {"template": "test/add_case.html"}, name="add"),
 )
