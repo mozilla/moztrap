d4a8d45aa265648d6a27004d444ddb6548f0074e
Carl Meyer <carl@oddbird.net>
2011-01-25 18:12:55 -0500

    Moved passwords-don't-match error to password2 field.

--- ui/tcmui/users/forms.py
+++ ui/tcmui/users/forms.py
@@ -59,16 +59,20 @@ class RegistrationForm(UserPlaceholdersMixin, RemoteObjectForm):
         super(RegistrationForm, self).__init__(*args, **kwargs)
 
 
-    def clean(self):
-        if "password" in self.cleaned_data and "password2" in self.cleaned_data:
-            if self.cleaned_data["password"] != self.cleaned_data["password2"]:
-                raise forms.ValidationError("Provided passwords do not match.")
-            del self.cleaned_data["password2"]
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password", "")
+        password2 = self.cleaned_data["password2"]
+        if password1 != password2:
+            raise forms.ValidationError("Provided passwords didn't match.")
+        return password2
 
+
+    def clean(self):
         if all(k in self.cleaned_data
                for k in ["firstName", "lastName", "screenName",
-                         "email", "password"]):
+                         "email", "password", "password2"]):
             data = self.cleaned_data
+            del data["password2"]
             data["company"] = self.company
             user = User(**data)
             UserList.get(auth=admin).post(user)
