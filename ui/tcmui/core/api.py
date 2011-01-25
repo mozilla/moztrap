b9921257aa5980400ee8c5656a026b3aea1ea9d9
Carl Meyer <carl@oddbird.net>
2011-01-25 12:15:48 -0500

    Updated RemoteObject to clear out any URL override in favor of resourceIdentity URL when an object is fetched.

--- ui/tcmui/core/api.py
+++ ui/tcmui/core/api.py
@@ -294,6 +294,12 @@ class RemoteObject(ObjectMixin, remoteobjects.RemoteObject):
     _location = property(_get_location, _set_location)
 
 
+    def update_from_response(self, url, response, content):
+        super(RemoteObject, self).update_from_response(url, response, content)
+        # If updated from a response, we should have a resourceIdentity.url;
+        # use that canonical URL rather than any previously-set URL.
+        self._location_override = None
+
     def update_from_dict(self, data):
         """
         Unwrap the JSON data.
