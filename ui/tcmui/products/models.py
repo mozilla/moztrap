b930c70706989d520a79f2c02010e8428ff05d98
Carl Meyer <carl@oddbird.net>
2011-02-02 11:18:47 -0500

    Added Product.autogenerate_env_groups method.

--- ui/tcmui/products/models.py
+++ ui/tcmui/products/models.py
@@ -4,7 +4,9 @@ Product-related remote objects.
 """
 from ..core.api import RemoteObject, ListObject, fields
 from ..core.models import Company
+from ..core.util import object_or_id
 from ..environments.models import EnvironmentGroupList
+
 
 
 class Product(RemoteObject):
@@ -17,6 +19,24 @@ class Product(RemoteObject):
 
     def __unicode__(self):
         return self.name
+
+
+    def autogenerate_env_groups(self, environments, envtype=None, **kwargs):
+        if envtype:
+            # support either a raw ID or an EnvironmentType object
+            try:
+                typeid = envtype.identity["@id"]
+            except (AttributeError, KeyError):
+                typeid = envtype
+            url = "environmentgroups/environmenttypes/%s/autogenerate" % typeid
+        else:
+            url = "environmentgroups/autogenerate"
+
+        extra_payload = {
+            "environmentIds": [object_or_id(e) for e in environments]}
+
+        self._put(relative_url=url, extra_payload=extra_payload, **kwargs)
+
 
 
 class ProductList(ListObject):
