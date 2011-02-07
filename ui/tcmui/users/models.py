7c8fda34c60742f8c8e02536d1985997288914bd
Carl Meyer <carl@oddbird.net>
2011-02-07 18:46:53 -0500

    Yeah, we need to submit the password of the new user.

--- ui/tcmui/users/models.py
+++ ui/tcmui/users/models.py
@@ -70,7 +70,7 @@ class User(Activatable, RemoteObject):
     email = fields.Field()
     firstName = fields.Field()
     lastName = fields.Field()
-    password = fields.Field(api_submit_name=False)
+    password = fields.Field()
     screenName = fields.Field()
     userStatus = StaticData("USERSTATUS")
 
