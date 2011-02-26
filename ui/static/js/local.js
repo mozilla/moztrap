4a3a0402c0c17e21a2e561220658f3027241b0de
Jonny Gerig Meyer <jonny@oddbird.net>
2011-02-26 13:10:35 -0500

    js addclass "hadfocus" on input, textarea blur

--- ui/static/js/local.js
+++ ui/static/js/local.js
@@ -1,4 +1,7 @@
 $(
     function() {
         $('input[placeholder], textarea[placeholder]').placeholder();
+        $('input, textarea').blur(function() {
+            $(this).addClass('hadfocus');
+        });
     });
