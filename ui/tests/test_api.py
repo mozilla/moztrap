558a496a730612e38638304c6cc649c9ff0fa7f4
Carl Meyer <carl@oddbird.net>
2011-04-23 20:01:30 -0500

    Test core API with fake resource class, not via Company.

--- ui/tests/test_api.py
+++ ui/tests/test_api.py
@@ -1,19 +1,24 @@
+import json
+
+import httplib
 from mock import patch, Mock
 from unittest2 import TestCase
+
+from .responses import response, make_identity, FakeResponse
+from .utils import ResourceTestCase
 
 
 
 class CachedHttpTest(TestCase):
     def make_request(self, **kwargs):
         from tcmui.core.api import CachedHttp
-        import httplib
 
-        response = Mock()
-        response.status = kwargs.pop("response_status", httplib.OK)
+        res = Mock()
+        res.status = kwargs.pop("response_status", httplib.OK)
         content = kwargs.pop("response_content", "content")
         with patch(
             "tcmui.core.api.httplib2.Http.request",
-            Mock(return_value=(response, content))):
+            Mock(return_value=(res, content))):
             return CachedHttp().request(**kwargs)
 
 
@@ -119,3 +124,279 @@ class CredentialsTest(TestCase):
         c = self.get_creds("user@example.com")
 
         self.assertEqual(repr(c), "<Credentials: user@example.com>")
+
+
+
+class TestResourceTestCase(ResourceTestCase):
+    RESOURCE_DEFAULTS = {
+        "name": "Default name",
+        }
+
+    RESOURCE_TYPE = "testresource"
+
+    RESOURCE_TYPE_PLURAL = "testresources"
+
+
+    def get_resource_class(self):
+        from tcmui.core.api import RemoteObject, fields
+
+        class TestResource(RemoteObject):
+            name = fields.Field()
+
+        return TestResource
+
+
+    def get_resource_list_class(self):
+        from tcmui.core.api import ListObject, fields
+
+        class TestResourceList(ListObject):
+            entryclass = self.resource_class
+            api_name = self.RESOURCE_TYPE_PLURAL
+            default_url = self.RESOURCE_TYPE_PLURAL
+
+            entries = fields.List(fields.Object(self.resource_class))
+
+        return TestResourceList
+
+
+
+@patch("remoteobjects.http.userAgent")
+class ResourceObjectTest(TestResourceTestCase):
+    def test_get_data(self, http):
+        http.request.return_value = response(
+            httplib.OK, self.make_one(name="Test TestResource"))
+
+        c = self.resource_class.get("testresources/3")
+
+        self.assertEqual(c.name, "Test TestResource")
+
+
+    def test_unicode_conversion(self, http):
+        http.request.return_value = response(
+            httplib.OK, self.make_one(name="Test TestResource"))
+
+        c = self.resource_class.get("testresources/3")
+
+        self.assertEqual(type(c.name), unicode)
+
+
+    def test_get_url(self, http):
+        http.request.return_value = response(
+            httplib.OK, self.make_one(name="Test TestResource"))
+
+        c = self.resource_class.get("testresources/3")
+        c.deliver()
+
+        self.assertEqual(
+            http.request.call_args[1]["uri"],
+            "http://fake.base/rest/testresources/3?_type=json")
+
+
+    def test_get_id(self, http):
+        http.request.return_value = response(
+            httplib.OK, self.make_one(
+                name="Test TestResource",
+                resourceIdentity=make_identity(id="3")))
+
+        c = self.resource_class.get("testresources/3")
+
+        self.assertEqual(c.id, "3")
+
+
+    def test_get_location(self, http):
+        http.request.return_value = response(
+            httplib.OK, self.make_one(
+                name="Test TestResource",
+                resourceIdentity=make_identity(url="testresources/3/")))
+
+        c = self.resource_class.get("testresources/3")
+        c.deliver()
+
+        self.assertEqual(c._location, "http://fake.base/rest/testresources/3/")
+
+
+    def test_create(self, http):
+        c = self.resource_class(name="Some TestResource")
+
+        self.assertEqual(c.name, "Some TestResource")
+
+
+    def test_no_auth_no_auth_headers(self, http):
+        http.request.return_value = response(
+            httplib.OK, self.make_one(name="Test TestResource"))
+
+        c = self.resource_class.get("testresources/3")
+        c.deliver()
+
+        headers = http.request.call_args[1]["headers"]
+        self.assertFalse("cookie" in headers)
+        self.assertFalse("authorization" in headers)
+
+
+    def test_auth_headers_password(self, http):
+        http.request.return_value = response(
+            httplib.OK, self.make_one(name="Test TestResource"))
+
+        c = self.resource_class.get(
+            "testresources/3",
+            auth=self.creds("user@example.com", password="blah"))
+        c.deliver()
+
+        headers = http.request.call_args[1]["headers"]
+        self.assertTrue("authorization" in headers)
+        self.assertFalse("cookie" in headers)
+
+
+    def test_auth_headers_cookie(self, http):
+        http.request.return_value = response(
+            httplib.OK, self.make_one(name="Test TestResource"))
+
+        c = self.resource_class.get(
+            "testresources/3",
+            auth=self.creds("user@example.com", cookie="USERTOKEN: blah"))
+        c.deliver()
+
+        headers = http.request.call_args[1]["headers"]
+        self.assertFalse("authorization" in headers)
+        self.assertTrue("cookie" in headers)
+
+
+    def test_get_persists_auth(self, http):
+        http.request.return_value = response(
+            httplib.OK, self.make_one(name="Test TestResource"))
+
+        creds = self.creds("user@example.com", cookie="USERTOKEN: blah")
+
+        c = self.resource_class.get("testresources/3", auth=creds)
+        c.deliver()
+
+        self.assertEqual(c.auth, creds)
+
+
+    def test_get_full_url(self, http):
+        http.request.return_value = response(
+            httplib.OK, self.make_one(name="Test TestResource"))
+
+        c = self.resource_class.get("http://some.other.url/testresources/3")
+        c.deliver()
+
+        self.assertEqual(
+            http.request.call_args[1]["uri"],
+            "http://some.other.url/testresources/3?_type=json")
+
+
+    def test_unauthorized(self, http):
+        http.request.return_value = response(
+            httplib.UNAUTHORIZED, "some error", {"content-type": "text/plain"})
+
+        c = self.resource_class.get("testresources/3")
+        with self.assertRaises(self.resource_class.Unauthorized) as cm:
+            c.deliver()
+
+        self.assertEqual(
+            cm.exception.args[0],
+            "401  requesting TestResource "
+            'http://fake.base/rest/testresources/3?_type=json: some error')
+
+
+    def test_json_error(self, http):
+        http.request.return_value = response(
+            httplib.CONFLICT, {"errors":[{"error":"email.in.use"}]})
+
+        c = self.resource_class.get("testresources/3")
+        with self.assertRaises(self.resource_class.Conflict) as cm:
+            c.deliver()
+
+        self.assertEqual(
+            cm.exception.args[0],
+            "409  requesting TestResource "
+            "http://fake.base/rest/testresources/3?_type=json: email.in.use")
+
+
+    def test_bad_response(self, http):
+        http.request.return_value = response(
+            777, "Something is very wrong.")
+
+        c = self.resource_class.get("testresources/3")
+        with self.assertRaises(self.resource_class.BadResponse) as cm:
+            c.deliver()
+
+        self.assertEqual(
+            cm.exception.args[0],
+            "Unexpected response requesting TestResource "
+            "http://fake.base/rest/testresources/3?_type=json: 777 ")
+
+
+    def test_missing_location_header(self, http):
+        http.request.return_value = response(
+            302, "")
+
+        c = self.resource_class.get("testresources/3")
+        with self.assertRaises(self.resource_class.BadResponse) as cm:
+            c.deliver()
+
+        self.assertEqual(
+            cm.exception.args[0],
+            "'Location' header missing from 302  response requesting TestResource "
+            "http://fake.base/rest/testresources/3?_type=json")
+
+
+    def test_bad_content_type(self, http):
+        http.request.return_value = response(
+            httplib.OK, "blah", {"content-type": "text/plain"})
+
+        c = self.resource_class.get("testresources/3")
+        with self.assertRaises(self.resource_class.BadResponse) as cm:
+            c.deliver()
+
+        self.assertEqual(
+            cm.exception.args[0],
+            "Bad response fetching TestResource "
+            "http://fake.base/rest/testresources/3?_type=json: "
+            "content-type text/plain is not an expected type")
+
+
+    def test_unicode_response(self, http):
+        http.request.return_value = (
+            FakeResponse(
+                httplib.OK,
+                headers={"content-type": "application/json"}),
+            unicode(json.dumps(self.make_one(name="Test TestResource")))
+            )
+
+        c = self.resource_class.get("testresources/3")
+
+        self.assertEqual(type(c.name), unicode)
+
+
+    def test_cache_attribute(self, http):
+        with patch.object(self.resource_class, "cache", True):
+            with patch("remoteobjects.RemoteObject.get") as mock:
+                self.resource_class.get("testresources/3")
+
+        from tcmui.core.api import cachedUserAgent
+
+        mock.assert_called_with('testresources/3', http=cachedUserAgent)
+
+
+
+@patch("remoteobjects.http.userAgent")
+class ListObjectTest(TestResourceTestCase):
+    def test_get_data_one(self, http):
+        http.request.return_value = response(
+            httplib.OK, self.make_searchresult({"name":"Test TestResource"}))
+
+        c = self.resource_list_class.get()
+
+        self.assertEqual(c[0].name, "Test TestResource")
+
+
+    def test_get_data_multiple(self, http):
+        http.request.return_value = response(
+            httplib.OK, self.make_searchresult(
+                {"name": "Test TestResource"},
+                {"name": "Second Test"}))
+
+        c = self.resource_list_class.get()
+
+        self.assertEqual(c[1].name, "Second Test")
---
+++ ui/tests/utils.py
@@ -0,0 +1,64 @@
+from unittest2 import TestCase
+
+from .responses import make_one, make_searchresult
+
+
+
+class ResourceTestCase(TestCase):
+    RESOURCE_DEFAULTS = {}
+
+    RESOURCE_TYPE = ""
+
+    RESOURCE_TYPE_PLURAL = ""
+
+
+    @property
+    def resource_class(self):
+        if not hasattr(self, "_resource_class"):
+            self._resource_class = self.get_resource_class()
+
+        return self._resource_class
+
+
+    def get_resource_class(self):
+        raise NotImplementedError
+
+
+    def make_one(self, **kwargs):
+        return {
+            "ns1.%s" % self.RESOURCE_TYPE: [
+                make_one(
+                    self.RESOURCE_TYPE,
+                    defaults=self.RESOURCE_DEFAULTS,
+                    **kwargs)
+                ]
+            }
+
+
+    @property
+    def resource_list_class(self):
+        if not hasattr(self, "_resource_list_class"):
+            self._resource_list_class = self.get_resource_list_class()
+
+        return self._resource_list_class
+
+
+    def get_resource_list_class(self):
+        raise NotImplementedError
+
+
+    def make_searchresult(self, *dicts):
+        return make_searchresult(
+            self.RESOURCE_TYPE,
+            self.RESOURCE_TYPE_PLURAL,
+            *[
+                make_one(
+                    self.RESOURCE_TYPE, defaults=self.RESOURCE_DEFAULTS, **info)
+                for info in dicts
+                ]
+            )
+
+
+    def creds(self, *args, **kwargs):
+        from tcmui.core.auth import Credentials
+        return Credentials(*args, **kwargs)
