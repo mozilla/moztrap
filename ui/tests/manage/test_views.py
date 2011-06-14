7c4411d1b2b78c4b491a884d7fef8d24946823c7
Carl Meyer <carl@oddbird.net>
2011-06-14 15:23:55 -0500

    Added first manage view test (home redirect).

---
+++ ui/tests/manage/test_views.py
@@ -0,0 +1,21 @@
+from mock import patch
+
+from ..testexecution.builders import testcycles
+from ..utils import ViewTestCase
+
+
+
+@patch("tcmui.core.api.userAgent")
+class DefaultManageViewTest(ViewTestCase):
+    @property
+    def view(self):
+        from tcmui.manage.views import home
+        return home
+
+
+    def test_redirect(self, http):
+        res = self.get("/manage/")
+
+        self.assertEqual(res.status_code, 302)
+        self.assertEqual(
+            res["Location"], "/manage/testcycles/")
