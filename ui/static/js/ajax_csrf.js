dc73ceed59b19192ef4ee8d6e5c291956103e007
Jonny Gerig Meyer <jonny@oddbird.net>
2011-07-13 22:58:06 -0400

    more JSLint-inspired cleanup

--- ui/static/js/ajax_csrf.js
+++ ui/static/js/ajax_csrf.js
@@ -1,16 +1,29 @@
-(function($) {
+/*jslint    browser:    true,
+            indent:     4,
+            confusion:  true
+*/
+/*global    ich,
+            jQuery
+*/
+
+(function ($) {
+
+    'use strict';
 
     // from http://docs.djangoproject.com/en/1.3/ref/contrib/csrf/#ajax
     $('html').ajaxSend(
-        function(event, xhr, settings) {
-            var getCookie = function(name) {
-                var cookieValue = null;
+        function (event, xhr, settings) {
+            var getCookie = function (name) {
+                var cookieValue = null,
+                    cookies,
+                    cookie,
+                    i;
                 if (document.cookie && document.cookie !== '') {
-                    var cookies = document.cookie.split(';');
-                    for (var i = 0; i < cookies.length; i++) {
-                        var cookie = $.trim(cookies[i]);
+                    cookies = document.cookie.split(';');
+                    for (i = 0; i < cookies.length; i = i + 1) {
+                        cookie = $.trim(cookies[i]);
                         // Does this cookie string begin with the name we want?
-                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        if (cookie.substring(0, name.length + 1) === (name + '=')) {
                             cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                             break;
                         }
@@ -25,4 +38,4 @@
         }
     );
 
-})(jQuery);
+}(jQuery));
--- ui/static/js/ajax_setup.js
+++ ui/static/js/ajax_setup.js
@@ -1,11 +1,23 @@
-(function($) {
+/*jslint    browser:    true,
+            indent:     4,
+            confusion:  true
+*/
+/*global    ich,
+            jQuery
+*/
+
+(function ($) {
+
+    'use strict';
+
     $("body").ajaxError(
-        function(event, request, settings, error) {
+        function (event, request, settings, error) {
             var data;
             $('body').loadingOverlay('remove');
-            if(error === "UNAUTHORIZED" || error === "FORBIDDEN") {
+            if (error === "UNAUTHORIZED" || error === "FORBIDDEN") {
                 data = $.parseJSON(request.responseText);
                 window.location = data.login_url + "?next=" + window.location.pathname;
             } // @@@ any global default error handling needed?
-        });
-})(jQuery);
+        }
+    );
+}(jQuery));
