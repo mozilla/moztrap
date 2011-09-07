089edfb3693a2114062ff340bcbeeb5a65879b2a
Jonny Gerig Meyer <jonny@oddbird.net>
2011-09-07 12:36:42 -0400

    jslint-inspired cleanup

--- jstests/tests.js
+++ jstests/tests.js
@@ -1,4 +1,10 @@
-$(function () {
+/*jslint    browser:    true,
+            indent:     4 */
+/*global    jQuery, module, test, equal */
+
+(function ($) {
+
+    'use strict';
 
     module('jquery.ellipsis.js', {
         setup: function () {
@@ -6,7 +12,7 @@ $(function () {
         }
     });
 
-    if (!('textOverflow' in document.documentElement.style || 'OTextOverflow' in document.documentElement.style)) {
+    if (document.documentElement.style.textOverflow === undefined && document.documentElement.style.OTextOverflow === undefined) {
 
         test('basic functionality in non-Webkit browsers', 1, function () {
             $('#qunit-fixture .ellipsis').ellipsis();
@@ -23,13 +29,13 @@ $(function () {
     } else {
         test('basic functionality in Webkit/Opera', 1, function () {
             $('#qunit-fixture .ellipsis').ellipsis();
-            if ('textOverflow' in document.documentElement.style) {
+            if (document.documentElement.style.textOverflow !== undefined) {
                 equal($('#qunit-fixture .ellipsis').css('text-overflow'), 'ellipsis', 'should set "text-overflow: ellipsis" in Webkit browsers');
             }
-            if ('OTextOverflow' in document.documentElement.style) {
+            if (document.documentElement.style.OTextOverflow !== undefined) {
                 equal($('#qunit-fixture .ellipsis').css('-o-text-overflow'), 'ellipsis', 'should set "-o-text-overflow: ellipsis" in Opera');
             }
         });
     }
 
-}());
+}(jQuery));
