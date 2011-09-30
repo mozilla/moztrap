4a2c155cd1259b17569ae624d20268d7e7edac42
Jonny Gerig Meyer <jonny@oddbird.net>
2011-09-30 14:38:13 -0400

    manage tags js: tag editing js in place (not functional)

--- static/js/manage-tags.js
+++ static/js/manage-tags.js
@@ -8,24 +8,70 @@ var CC = (function (CC, $) {
 
     CC.manageTags = function (container) {
         var context = $(container),
-            addTag = context.find('#add-tag-form'),
-            replaceList = addTag.closest('.action-ajax-replace'),
-            editTag = context.find('.item a[title="edit"]');
 
-        addTag.ajaxForm({
-            beforeSubmit: function (arr, form, options) {
-                replaceList.loadingOverlay();
+            addTag = function () {
+                var addTagForm = context.find('#add-tag-form'),
+                    replaceList = addTagForm.closest('.action-ajax-replace');
+
+                addTagForm.ajaxForm({
+                    beforeSubmit: function (arr, form, options) {
+                        replaceList.loadingOverlay();
+                    },
+                    success: function (response) {
+                        var newList = $(response.html);
+                        replaceList.loadingOverlay('remove');
+                        if (response.html) {
+                            replaceList.replaceWith(newList);
+                            addTag();
+                            newList.find('.details').html5accordion();
+                        }
+                    }
+                });
             },
-            success: function (response) {
-                var newList = $(response.html);
-                replaceList.loadingOverlay('remove');
-                if (response.html) {
-                    replaceList.replaceWith(newList);
-                    CC.manageTags('#managetags');
-                    newList.find('.details').html5accordion();
-                }
+
+            editTag = function () {
+                var editTagForm = context.find('#edit-tag-form'),
+                    replaceList = editTagForm.closest('.action-ajax-replace');
+
+                editTagForm.ajaxForm({
+                    beforeSubmit: function (arr, form, options) {
+                        replaceList.loadingOverlay();
+                    },
+                    success: function (response) {
+                        var newList = $(response.html);
+                        replaceList.loadingOverlay('remove');
+                        if (response.html) {
+                            replaceList.replaceWith(newList);
+                            newList.find('.details').html5accordion();
+                        }
+                    }
+                });
+            };
+
+        context.delegate('.item a[title="edit"]', 'click', function (e) {
+            e.preventDefault();
+            var thisTag = $(this).closest('.item'),
+                tagID = thisTag.data('id'),
+                tagName = thisTag.find('h3.tag').text(),
+                editForm = ich.tag_edit({
+                    id: tagID,
+                    name: tagName
+                }),
+                cancel;
+
+            if (tagID && tagName) {
+                thisTag.replaceWith(editForm);
+                editTag();
+                cancel = $(editForm).find('a[title="cancel"]');
+
+                cancel.click(function (e) {
+                    e.preventDefault();
+                    $(editForm).replaceWith(thisTag);
+                });
             }
         });
+
+        addTag();
     };
 
     return CC;
