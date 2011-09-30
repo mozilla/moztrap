9694186ad590cd4882fbde74c9f31566f92685c2
Carl Meyer <carl@oddbird.net>
2011-09-30 14:31:30 -0600

    Revert "testcase attachments js clones existing input; no need for ich template"
    This reverts commit fcc047a78845dc9887ce51c00e53cdc16750621b.

--- static/js/manage-results.js
+++ static/js/manage-results.js
@@ -750,7 +750,7 @@ var CC = (function (CC, $) {
                 name: filename,
                 input: inputID
             });
-            newInput = input.clone().attr('id', 'attachment-input-' + counter);
+            newInput = ich.case_attachment_input({ counter: counter });
             uploads.append(attachment);
             context.append(newInput);
             label.attr('for', 'attachment-input-' + counter);
