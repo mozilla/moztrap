5960f05cc7f12f53e37e41662e35967c2f876f55
Carl Meyer <carl@oddbird.net>
2012-01-25 10:18:59 -0700

    Fixed pagination preservation on re-filter.

--- cc/view/lists/templatetags/pagination.py
+++ cc/view/lists/templatetags/pagination.py
@@ -33,6 +33,7 @@ register = Library()
 
 
 class Paginate(Tag):
+    """Paginate the given queryset, placing a Pager in the template context."""
     name = "paginate"
     options = Options(
         Argument("queryset"),
@@ -42,6 +43,7 @@ class Paginate(Tag):
 
 
     def render_tag(self, context, queryset, varname):
+        """Place Pager for given ``queryset`` in context as ``varname``."""
         request = context["request"]
         pagesize, pagenum = pagination.from_request(request)
         context[varname] = pagination.Pager(queryset, pagesize, pagenum)
@@ -54,10 +56,24 @@ register.tag(Paginate)
 
 @register.filter
 def pagenumber_url(request, pagenumber):
+    """Return current full URL with pagenumber replaced."""
     return pagination.pagenumber_url(request.get_full_path(), pagenumber)
 
 
 
 @register.filter
 def pagesize_url(request, pagesize):
+    """Return current full URL with pagesize replaced."""
     return pagination.pagesize_url(request.get_full_path(), pagesize)
+
+
+@register.filter
+def pagenumber(request):
+    """Return pagenumber of given request."""
+    return pagination.from_request(request)[1]
+
+
+@register.filter
+def pagesize(request):
+    """Return pagesize of given request."""
+    return pagination.from_request(request)[0]
