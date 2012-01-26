ff378445ea33e7d518737e12d37ee03ee3887f78
Carl Meyer <carl@oddbird.net>
2012-01-26 12:24:13 -0700

    Run tests through coverage, use .coveragerc for options.

--- cc/model/core/management/commands/test.py
+++ cc/model/core/management/commands/test.py
@@ -26,14 +26,6 @@ from south.management.commands import patch_for_test_db_setup
 
 
 class Command(TestCommand):
-    option_list = TestCommand.option_list + (
-        make_option(
-            '--coverage', action='store_true', dest='coverage', default=False,
-            help='Report test coverage in htmlcov/.'),
-        make_option(
-            '--coverall', action='store_true', dest='coverall', default=False,
-            help='Report test coverage of all modules, even if not imported.'),
-        )
     help = 'Runs the test modules, cases, or methods specified by dotted path on the command line, or does test discovery if no arguments are given.'
     args = '[dotted-path ...]'
 
@@ -41,10 +33,6 @@ class Command(TestCommand):
 
     def handle(self, *test_labels, **options):
         patch_for_test_db_setup()
-
-        if options.get("coverage"):
-            cov = coverage.coverage(branch=True)
-            cov.start()
 
         from django.conf import settings
         from django.test.utils import get_runner
@@ -57,39 +45,4 @@ class Command(TestCommand):
         test_runner = TestRunner(verbosity=verbosity, interactive=interactive, failfast=failfast)
         failures = test_runner.run_tests(test_labels)
 
-        if options.get("coverage"):
-            cov.stop()
-
-            include = []
-            omit = [
-                "*/migrations/*",
-                "cc/deploy/wsgi.py",
-                "cc/deploy/vendor_wsgi.py"
-                ]
-            report_kw = {}
-            if options.get("coverall"):
-                # include all python modules under cc in report, including even
-                # those the tests don't cause to be imported at all yet, so as
-                # to get a more accurate overall coverage figure.
-                py_files = include
-                for dirpath, dirs, filenames in os.walk(
-                        os.path.join(settings.BASE_PATH, "cc")):
-                    py_files.extend([os.path.join(dirpath, fn)
-                                     for fn in filenames
-                                     if fn.endswith(".py")
-                                     and not fn.startswith(".")])
-                report_kw["morfs"] = py_files
-                report_kw["omit"] = omit + [
-                    "cc/model/core/management/commands/test.py",
-                    "cc/settings/base.py",
-                    "cc/settings/default.py",
-                    "cc/settings/local.py",
-                    "cc/settings/local.sample.py"]
-            else:
-                report_kw["include"] = include + [
-                    os.path.join(settings.BASE_PATH, "cc/*")]
-                report_kw["omit"] = omit
-            cov.html_report(**report_kw)
-
-        if failures:
-            sys.exit(bool(failures))
+        sys.exit(bool(failures))
