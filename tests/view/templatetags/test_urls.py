0a16a5d79b3e652d208bf751dd497df88e397d16
Carl Meyer <carl@oddbird.net>
2012-02-07 16:10:22 -0700

    Add test for protocol template tag.

--- tests/view/templatetags/test_urls.py
+++ tests/view/templatetags/test_urls.py
@@ -19,7 +19,11 @@
 Tests for URL-related template filters.
 
 """
+from django.template import Template, Context
 from django.utils.unittest import TestCase
+
+# @@@ import from Django in 1.4
+from djangosecure.test_utils import override_settings
 
 
 
@@ -40,3 +44,24 @@ class FilterTest(TestCase):
     def test_is_not_url(self):
         """is_url filter detects a non-URL."""
         self.assertFalse(self.urls.is_url("1234567"))
+
+
+
+class ProtocolTest(TestCase):
+    """Tests for the protocol template tag."""
+    def tag(self):
+        """Return the output of the {% protocol %} template tag."""
+        t = Template("{% load urls %}{% protocol %}")
+        return t.render(Context({}))
+
+
+    @override_settings(SESSION_COOKIE_SECURE=False)
+    def test_http(self):
+        """protocol tag returns 'http' if SESSION_COOKIE_SECURE is False."""
+        self.assertEqual(self.tag(), "http")
+
+
+    @override_settings(SESSION_COOKIE_SECURE=True)
+    def test_https(self):
+        """protocol tag returns 'https' if SESSION_COOKIE_SECURE is True."""
+        self.assertEqual(self.tag(), "https")
