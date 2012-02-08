6961b8878f1da5329b4c219f78c66dff188ec096
Carl Meyer <carl@oddbird.net>
2012-02-08 15:02:30 -0700

    More extensive docstring for base test case classes.

--- tests/case/__init__.py
+++ tests/case/__init__.py
@@ -18,6 +18,15 @@
 """
 Utility base TestCase classes for Case Conductor.
 
+django.utils.unittest.TestCase provides a bare TestCase class for tests that
+don't access the database or need any of the other Django TestCase class
+utilities.
+
+DBTestCase is a Django TestCase, plus some CC-specific helpers.
+
+View and admin test case classes (using WebTest) are available from the view
+and admin sub-modules.
+
 """
 from django.utils.unittest import TestCase
 
