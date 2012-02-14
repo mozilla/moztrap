0287c02bf6299381cb190789dea61f4582f99270
Carl Meyer <carl@oddbird.net>
2012-02-13 17:03:21 -0700

    Add run-results list.

---
+++ cc/view/results/runs/views.py
@@ -0,0 +1,65 @@
+# Case Conductor is a Test Case Management system.
+# Copyright (C) 2011-2012 Mozilla
+#
+# This file is part of Case Conductor.
+#
+# Case Conductor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Case Conductor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Case Conductor.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Results views for runs.
+
+"""
+from django.shortcuts import get_object_or_404
+from django.template.response import TemplateResponse
+
+from django.contrib.auth.decorators import login_required
+
+from cc import model
+
+from cc.view.filters import RunFilterSet
+from cc.view.lists import decorators as lists
+from cc.view.utils.ajax import ajax
+
+# @@@ from ..finders import ResultsFinder
+
+
+
+@login_required
+# @@@ @lists.finder(ResultsFinder)
+@lists.filter("runs", filterset_class=RunFilterSet)
+@lists.sort("runs")
+@ajax("results/run/list/_runs_list.html")
+def runs_list(request):
+    """List runs."""
+    return TemplateResponse(
+        request,
+        "results/run/runs.html",
+        {
+            "runs": model.Run.objects.select_related(),
+            }
+        )
+
+
+
+@login_required
+def run_details(request, run_id):
+    """Get details snippet for a run."""
+    run = get_object_or_404(
+        model.Run, pk=run_id)
+    return TemplateResponse(
+        request,
+        "results/run/list/_run_details.html",
+        {
+            "run": run
+            }
+        )
