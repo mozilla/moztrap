d66d2a6cecc72e5a0eb9a9f9197f460e4c68ecce
Carl Meyer <carl@oddbird.net>
2012-06-12 15:31:33 -0600

    Case-id-redirect filters target list by case id to avoid pagination issues.

--- tests/view/manage/cases/test_views.py
+++ tests/view/manage/cases/test_views.py
@@ -283,7 +283,8 @@ class CaseTest(case.view.AuthenticatedViewTestCase):
 
         self.assertRedirects(
             res,
-            "{0}#caseversion-id-{1}".format(reverse("manage_cases"), cv.id),
+            "{0}?filter-id={1}#caseversion-id-{2}".format(
+                reverse("manage_cases"), cv.case.id, cv.id),
             )
 
 
@@ -304,7 +305,8 @@ class CaseTest(case.view.AuthenticatedViewTestCase):
 
         self.assertRedirects(
             res,
-            "{0}#caseversion-id-{1}".format(reverse("manage_cases"), cv.id),
+            "{0}?filter-id={1}#caseversion-id-{2}".format(
+                reverse("manage_cases"), cv.case.id, cv.id),
             )
 
 
