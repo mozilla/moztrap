073389600eafffa039fbe0a18181ef3e201e1943
Cameron Dawson <cdawson@mozilla.com>
2012-11-02 11:48:00 -0700

    cleaned up the template code and added new tag for fetching the summary result, which may be from another user

--- moztrap/view/runtests/templatetags/execution.py
+++ moztrap/view/runtests/templatetags/execution.py
@@ -11,6 +11,26 @@ from .... import model
 register = template.Library()
 
 
+def get_result(runcaseversion, user, environment):
+    result_kwargs = dict(
+        environment=environment,
+        tester=user,
+        runcaseversion=runcaseversion,
+        is_latest=True,
+        )
+    try:
+        result = model.Result.objects.get(**result_kwargs)
+    except model.Result.DoesNotExist:
+        result = model.Result(**result_kwargs)
+    except model.Result.MultipleObjectsReturned:
+        # find the latest one and set it to latest, which will set all
+        # others to is_latest=False
+        result = model.Result.objects.filter(**result_kwargs).order_by(
+            "-modified_on")[0]
+        result.set_latest()
+        result.save()
+    return result
+
 
 class ResultFor(Tag):
     """
@@ -32,25 +52,25 @@ class ResultFor(Tag):
 
     def render_tag(self, context, runcaseversion, user, environment, varname):
         """Get/construct Result and place it in context under ``varname``"""
-        result_kwargs = dict(
-            environment=environment,
-            tester=user,
-            runcaseversion=runcaseversion,
-            is_latest=True,
-            )
-        try:
-            result = model.Result.objects.get(**result_kwargs)
-        except model.Result.DoesNotExist:
-            result = model.Result(**result_kwargs)
-        except model.Result.MultipleObjectsReturned:
-            # find the latest one and set it to latest, which will set all
-            # others to is_latest=False
-            result = model.Result.objects.filter(**result_kwargs).order_by(
-                "-modified_on")[0]
-            result.set_latest()
-            result.save()
+#        result_kwargs = dict(
+#            environment=environment,
+#            tester=user,
+#            runcaseversion=runcaseversion,
+#            is_latest=True,
+#            )
+#        try:
+#            result = model.Result.objects.get(**result_kwargs)
+#        except model.Result.DoesNotExist:
+#            result = model.Result(**result_kwargs)
+#        except model.Result.MultipleObjectsReturned:
+#            # find the latest one and set it to latest, which will set all
+#            # others to is_latest=False
+#            result = model.Result.objects.filter(**result_kwargs).order_by(
+#                "-modified_on")[0]
+#            result.set_latest()
+#            result.save()
 
-        context[varname] = result
+        context[varname] = get_result(runcaseversion, user, environment)
         return u""
 
 
@@ -58,44 +78,93 @@ register.tag(ResultFor)
 
 
 
-class AlreadyTested(Tag):
+class SummaryResultFor(Tag):
     """
-    Places whether other user has a result for this runcaseversion/env in context.
+    Places Result for this runcaseversion/user/env in context.
 
-    If no relevant Result exists, returns false
+    If no relevant Result exists, returns *unsaved* default Result for use in
+    template (result will be saved when case is started.)
 
     """
-    name = "already_tested"
+    name = "summary_result_for"
     options = Options(
         Argument("runcaseversion"),
         Argument("user"),
         Argument("environment"),
         "as",
         Argument("varname", resolve=False),
+        "and",
+        Argument("summary_varname", resolve=False)
         )
 
 
-    def render_tag(self, context, runcaseversion, user, environment, varname):
+    def render_tag(self, context, runcaseversion, user, environment, varname,
+                   summary_varname):
         """Get/construct Result and place it in context under ``varname``"""
-        include_kwargs = dict(
-            environment=environment,
-            runcaseversion=runcaseversion,
-            is_latest=True,
-            )
-        exclude_kwargs = dict(
-            tester=user,
-        )
-        try:
-            result = model.Result.objects.filter(
-                **include_kwargs).exclude(**exclude_kwargs).order_by(
-                "-modified_on")[0]
-            context[varname] = result
-        except IndexError:
-            context[varname] = None
+
+        result =  get_result(runcaseversion, user, environment)
+        context[varname] = result
+
+        summary_result = result
+
+        # if the result.status is pending or assigned, then we try to find a result
+        # from another user to return instead.
+        if result.status == result.STATUS.assigned or result.status == result.STATUS.started:
+            include_kwargs = dict(
+                environment=environment,
+                runcaseversion=runcaseversion,
+                is_latest=True,
+                )
+            exclude_kwargs = dict(
+                tester=user,
+                )
+
+            try:
+                summary_result = model.Result.objects.filter(
+                    **include_kwargs).exclude(**exclude_kwargs).order_by(
+                    "-modified_on")[0]
+            except IndexError:
+                pass
+
+
+        context[summary_varname] = summary_result
         return u""
 
 
-register.tag(AlreadyTested)
+register.tag(SummaryResultFor)
+
+
+
+class ResultTitle(Tag):
+    """
+    Returns the title test that should show over the displayed result
+
+    Depends if there is no result yet, or if its not tested by user
+    and IS tested by another user
+
+    """
+    name = "result_title"
+    options = Options(
+        Argument("result"),
+        Argument("user"),
+        "as",
+        Argument("varname", resolve=False),
+        )
+
+
+    def render_tag(self, context, result, user, varname):
+        """Get/construct text and place it in context under ``varname``"""
+
+        if result.status == result.STATUS.assigned or result.status == result.STATUS.started:
+            context[varname] = "not yet tested"
+        elif result.tester == user:
+            context[varname] = "{0} by you".format(result.status)
+        else:
+            context[varname] = "{0} by {1} - click to submit your own result".format(result.status, result.tester)
+        return u""
+
+
+register.tag(ResultTitle)
 
 
 
