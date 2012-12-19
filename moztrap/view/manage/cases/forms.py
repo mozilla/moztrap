b0e533c8ea5f8c1b822aec4aee61c7caf874e183
Cameron Dawson <cdawson@mozilla.com>
2012-12-18 18:28:19 -0800

    fixed bug where create new case doesn't add the case to the END of the suite

--- moztrap/view/manage/cases/forms.py
+++ moztrap/view/manage/cases/forms.py
@@ -4,7 +4,7 @@ Management forms for cases.
 """
 from django.core.urlresolvers import reverse
 from django.forms.models import inlineformset_factory, BaseInlineFormSet
-
+from django.db.models import Max
 import floppyforms as forms
 
 from .... import model
@@ -198,8 +198,15 @@ class AddCaseForm(BaseAddCaseForm, BaseCaseVersionForm, BaseCaseForm):
 
         initial_suite = version_kwargs.pop("initial_suite", None)
         if initial_suite:
+            order = model.SuiteCase.objects.filter(
+                suite=initial_suite,
+                ).aggregate(Max("order"))["order__max"] or 0
             model.SuiteCase.objects.create(
-                case=case, suite=initial_suite, user=self.user)
+                case=case,
+                suite=initial_suite,
+                user=self.user,
+                order=order+1,
+                )
 
         productversions = [version_kwargs.pop("productversion")]
         if version_kwargs.pop("and_later_versions"):
@@ -271,6 +278,12 @@ class AddBulkCaseForm(BaseAddCaseForm, BaseCaseForm):
 
         cases = []
 
+        order = 0
+        if initial_suite:
+            order = model.SuiteCase.objects.filter(
+                suite=initial_suite,
+                ).aggregate(Max("order"))["order__max"] or 0
+
         for case_data in self.cleaned_data["cases"]:
             case = model.Case.objects.create(
                 product=product,
@@ -286,8 +299,13 @@ class AddBulkCaseForm(BaseAddCaseForm, BaseCaseForm):
             version_kwargs["user"] = self.user
 
             if initial_suite:
+                order += 1
                 model.SuiteCase.objects.create(
-                    case=case, suite=initial_suite, user=self.user)
+                    case=case,
+                    suite=initial_suite,
+                    user=self.user,
+                    order=order,
+                    )
 
             for productversion in productversions:
                 this_version_kwargs = version_kwargs.copy()
